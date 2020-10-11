81b517ecbf7d19883a76e6464271f7132c69d83e
Ronald Russo <RonaldRusso94@yahoo.com>
2020-10-10 22:35:00 -0400

    Created Layout component and Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Layout from '../components/layout/Layout';
 
 export default function Home() {
   return (
@@ -8,9 +9,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-blue-900 h-screen text-center text-white">
-        <p>Main Content</p>
-      </main>
+      <Layout>
+        <main className="h-screen text-center ">
+          <p>Main Content</p>
+          <div className="w-6/12"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio ducimus quam dicta cupiditate tempore eos ea accusamus labore quis, voluptatem quod. Distinctio beatae perferendis quia rem soluta velit eaque natus.</p></div>
+        </main>
+      </Layout>
 
       <footer className="py-16 text-center">
         <p>Footer</p>

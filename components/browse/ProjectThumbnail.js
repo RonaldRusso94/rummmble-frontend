84031a8d5abb180eefbb31487220273af94859a1
ShinteiMai <stevenhanselgo@gmail.com>
2020-10-14 23:21:26 +0700

    :fire: Worked on ProjectThumbnail

--- components/browse/ProjectThumbnail.js
+++ components/browse/ProjectThumbnail.js
@@ -3,8 +3,11 @@ import PropTypes from "prop-types";
 
 export default function ProjectThumbnail() {
   return (
-    <div>
-      <img src="" />
+    <div className="h-64 w-full object-center relative">
+      <img
+        className="h-full w-full object-cover rounded-lg"
+        src="https://cdn.dribbble.com/users/427857/screenshots/14384008/media/48afb010968fbfcda13b015ee39fa6c6.png"
+      />
     </div>
   );
 }

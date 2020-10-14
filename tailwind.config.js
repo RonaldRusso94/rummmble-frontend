1e9554a59a65d9104e4eba12dfb2d5a5135ea57a
Ronald Russo <RonaldRusso94@yahoo.com>
2020-10-14 19:07:13 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   purge: [
     './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}'
+    './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
